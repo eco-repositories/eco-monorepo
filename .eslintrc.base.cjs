8889b70e96e1b2197647928d219637cfaecd3d9c
Dima Parzhitsky <parzhitsky@gmail.com>
2023-05-25 23:36:31 +0300

    Lint eslint config itself

--- .eslintrc.base.cjs
+++ .eslintrc.base.cjs
@@ -1,31 +1,33 @@
 module.exports = {
   root: true,
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "standard-with-typescript",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard-with-typescript',
   ],
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["./tsconfig.json"],
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   ignorePatterns: [
-    "node_modules",
-    "dist",
-    ".eslintrc.*",
+    'node_modules',
+    'dist',
+    '.eslintrc.*',
   ],
   rules: {
-    "comma-dangle": "off",
-    "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
 
-    "indent": "off",
-    "@typescript-eslint/indent": ["error", 2],
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
 
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/space-before-function-paren': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
 
     'semi': 'off',
+    'space-before-function-paren': 'off',
   },
-};
+}
