696191797204b62b1ff0de11f71bbec548d7ec7f
Dima Parzhitsky <parzhitsky@gmail.com>
2023-06-04 15:19:39 +0300

    Drop testFilePattern

--- packages/server/vitest.config.ts
+++ packages/server/vitest.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vitest/config'
 
-/** @private */
-const testPathPattern = /(?<suffix>\.(?:e2e-)?spec)(?<extension>\.ts)$/
-
 export default defineConfig({
   test: {
     root: __dirname,
     globals: true,
     mockReset: true,
     resolveSnapshotPath(path, extension) {
-      return path.replace(testPathPattern, `${extension}$2`)
+      return path
+        .replace('.spec', extension)
+        .replace('.e2e-spec', extension)
     },
   },
 })
