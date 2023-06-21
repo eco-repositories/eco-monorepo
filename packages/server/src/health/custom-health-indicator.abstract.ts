7d4e034bbc7d2fd4f21fb1dde7bec211723f88bc
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-06-21 17:02:03 +0300

    Generalize health indicators

--- packages/server/src/health/custom-health-indicator.abstract.ts
+++ packages/server/src/health/custom-health-indicator.abstract.ts
@@ -1,6 +1,8 @@
 import { HealthIndicator, type HealthIndicatorResult } from '@nestjs/terminus'
 
-export abstract class CustomHealthIndicator<Data extends object | undefined = undefined> extends HealthIndicator {
+export abstract class CustomHealthIndicator<
+  Data extends object | undefined = undefined,
+> extends HealthIndicator implements CustomHealthIndicator.WithCheckHealth {
   protected abstract readonly statusKey: string
 
   protected abstract isHealthy(): boolean | PromiseLike<boolean>
@@ -16,5 +18,13 @@ export abstract class CustomHealthIndicator<Data extends object | undefined = un
     const result = this.getStatus(key, isHealthy, data)
 
     return result
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace CustomHealthIndicator {
+  export interface WithCheckHealth {
+    // eslint-disable-next-line @typescript-eslint/method-signature-style
+    checkHealth(): HealthIndicatorResult | PromiseLike<HealthIndicatorResult>
   }
 }
--- packages/server/src/health/health.service.ts
+++ packages/server/src/health/health.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common'
-import { type HealthCheckResult, HealthCheckService } from '@nestjs/terminus'
+import { type HealthCheckResult, HealthCheckService, type HealthIndicatorFunction } from '@nestjs/terminus'
 import { BasicHealthIndicator } from './basic.health.js'
+import { type CustomHealthIndicator } from './custom-health-indicator.abstract.js'
 
 @Injectable()
 export class HealthService {
+  protected readonly healthIndicators: CustomHealthIndicator.WithCheckHealth[] = [
+    this.basicHealth,
+  ]
+
+  protected readonly healthIndicatorFunctions: HealthIndicatorFunction[] =
+    this.healthIndicators.map((indicator) => async () => await indicator.checkHealth())
+
   constructor(
     protected readonly healthCheckService: HealthCheckService,
     protected readonly basicHealth: BasicHealthIndicator,
   ) {}
 
   async getHealthCheck(): Promise<HealthCheckResult> {
-    const healthCheck = await this.healthCheckService.check([
-      async () => await this.basicHealth.checkHealth(),
-    ])
+    const healthCheck = await this.healthCheckService.check(this.healthIndicatorFunctions)
 
     return healthCheck
   }
