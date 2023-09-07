a4fbcc0c2460fe678d60ed9f44d0b046323d49c5
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-09-07 11:41:56 +0300

    Work with URLs using `url` module

--- packages/server/src/config/config.module.ts
+++ packages/server/src/config/config.module.ts
@@ -1,15 +1,18 @@
 import { Global, Module } from '@nestjs/common'
 import { ConfigModule as NestConfigModule } from '@nestjs/config'
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
 import { ConfigService } from './config.service.js'
 import { validateConfig } from './validate-config.js'
+
+/** @private */
+const envFileUrl = new URL('../../.env.local', import.meta.url)
 
 @Global()
 @Module({
   imports: [
     NestConfigModule.forRoot({
       envFilePath: [
-        resolve(import.meta.url, '../..', '.env.local'),
+        fileURLToPath(envFileUrl),
       ],
       validate: validateConfig,
     }),
