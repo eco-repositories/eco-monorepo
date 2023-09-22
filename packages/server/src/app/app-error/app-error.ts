b5abc7b7b3941cc5dd024626b87a55b258c2565f
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-09-22 13:13:14 +0300

    Fix improper module renaming

--- packages/server/src/app/app-error/app-error.ts
+++ packages/server/src/app/app-error/app-error.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'crypto'
-import { type HttpErrorResponseDetail as Detail } from '@@shared/api/http-error-response/http-error-response.type.js'
+import { type HttpErrorResponseDetail as Detail } from '@@shared/api/http-error-response-body/http-error-response-body.type.js'
 
 /** @private */
 type Digit = `${0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}`
