e06873bffad99e5c8235cd7db0c9640d5f537b42
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-09-29 23:28:39 +0300

    Migrate from Sequelize to TypeOrm

--- packages/server/src/db/db.health.ts
+++ packages/server/src/db/db.health.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common'
-import { type HealthIndicatorResult, SequelizeHealthIndicator, type SequelizePingCheckSettings } from '@nestjs/terminus'
+import { type HealthIndicatorResult, TypeOrmHealthIndicator, type TypeOrmPingCheckSettings } from '@nestjs/terminus'
 import { type CustomHealthIndicator } from '@/health/custom-health-indicator.abstract.js'
 
 @Injectable()
-export class DbHealthIndicator extends SequelizeHealthIndicator implements CustomHealthIndicator.WithCheckHealth {
+export class DbHealthIndicator extends TypeOrmHealthIndicator implements CustomHealthIndicator.WithCheckHealth {
   protected readonly statusKey: string = 'db'
-  protected readonly pingCheckOptions: SequelizePingCheckSettings = {
+  protected readonly pingCheckOptions: TypeOrmPingCheckSettings = {
     timeout: 2000,
   }
 
