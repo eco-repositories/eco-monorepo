226ad6254856ed51cd1149e4b199f1a1d186b42f
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-10-13 00:06:09 +0300

    Define `Pagination` and related types

--- packages/shared/src/api/api.type.ts
+++ packages/shared/src/api/api.type.ts
@@ -1,12 +1,14 @@
 import { type HealthCheckResult } from './health-check-result/health-check-result.type.js'
 import { type HttpErrorResponseDetail, type HttpErrorResponseBody } from './http-error-response-body/http-error-response-body.type.js'
-import { type HttpResponseBody } from './http-response-body/http-response-body.type.js'
+import { type HttpResponseBody, type HttpResponseBodyList, type HttpResponseBodyListPaginated } from './http-response-body/http-response-body.type.js'
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Api {
     export { type HealthCheckResult }
     export { type HttpResponseBody }
+    export { type HttpResponseBodyList }
+    export { type HttpResponseBodyListPaginated }
     export { type HttpErrorResponseBody }
     export { type HttpErrorResponseDetail }
   }
