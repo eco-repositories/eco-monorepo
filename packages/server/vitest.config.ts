a2522820bc7c4874b1098dcdc65a7e0deccd3f20
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-12-05 10:42:47 +0200

    Add mapping for '@@/*' paths on server

--- packages/server/vitest.config.ts
+++ packages/server/vitest.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
         find: /^@\/(.*)$/,
         replacement: resolve(__dirname, 'src/$1'),
       },
+      {
+        find: /^@@\/(.*)$/,
+        replacement: __dirname,
+      },
     ],
   },
   test: {
