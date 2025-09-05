b96a0e2fdba2bfa43f2c426b3513fb3e91342d93
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-09-05 18:32:06 +0300

    Move `ConfigModule` in `shared`

--- packages/server/src/config/config.module.ts
+++ packages/server/src/config/config.module.ts
@@ -1,20 +1,13 @@
 import { Global, Module } from '@nestjs/common'
-import { ConfigModule as NestConfigModule } from '@nestjs/config'
-import { fileURLToPath } from 'url'
+import { ConfigModule as SharedConfigModule } from '@@shared/config/config.module.js'
+import { ConfigDTO } from './config.dto.js'
 import { ConfigService } from './config.service.js'
-import { validateConfig } from './validate-config.js'
-
-/** @private */
-const envFileUrl = new URL('../../.env.local', import.meta.url)
 
 @Global()
 @Module({
   imports: [
-    NestConfigModule.forRoot({
-      envFilePath: [
-        fileURLToPath(envFileUrl),
-      ],
-      validate: validateConfig,
+    SharedConfigModule.forRoot(ConfigDTO, {
+      envFileUrl: new URL('../../.env.local', import.meta.url),
     }),
   ],
   providers: [ConfigService],
