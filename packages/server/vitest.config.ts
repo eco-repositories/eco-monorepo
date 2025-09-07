6e4d812e1720060a931b35390a6e0d8f604397c6
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-09-07 03:09:44 +0300

    Remove annoying dependency

--- packages/server/vitest.config.ts
+++ packages/server/vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config'
-import { esbuildDecorators } from '@anatine/esbuild-decorators'
+import esbuildPluginTsc from 'esbuild-plugin-tsc'
 import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
@@ -22,28 +22,8 @@ export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
-        // The `esbuildDecorators(…)` call below might show compiler error, which the instructions below might help to understand.
-        // Don't remove the comment: even if it seems to be outdated, it still might be able to nudge in the right direction.
-        /*
-          The original error:
-            'esbuildDecorators' function returns something that 'plugins' array cannot accept
-              'esbuildDecorators' function returns 'string' (deeply inside an object)
-                node_modules/esbuild[v0.19.2]/lib/main.d.ts
-                -> TsconfigRaw.compilerOptions.baseUrl
-                -> 'string'
-              'plugins' array accepts 'boolean' (deeply inside an object)
-                node_modules/vite[v4.4.9]/node_modules/esbuild[v0.18.20]/lib/main.d.ts
-                -> TsconfigRaw.compilerOptions.baseUrl
-                -> 'boolean'
-                  See https://github.com/evanw/esbuild/pull/3299
-          The solution:
-            Ideal:
-              @anatine/esbuild-decorators should depend on the version of esbuild with baseUrl being a string
-            Temporary:
-              Manually override esbuild to be of the same after-the-fix version for everyone
-         */
-        esbuildDecorators({
-          tsconfig: resolve(rootDir, 'tsconfig.json'),
+        esbuildPluginTsc({
+          tsconfigPath: resolve(rootDir, 'tsconfig.json'),
         }),
       ],
     },
