3a44c71c67658c3c0b1512db138a19448629bff6
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-09-08 12:36:02 +0300

    Fix test on Windows

--- packages/shared/src/resolve-path/resolve-path.spec.ts
+++ packages/shared/src/resolve-path/resolve-path.spec.ts
@@ -1,9 +1,13 @@
+import { resolve } from 'path'
+import { pathToFileURL } from 'url'
 import { resolvePath } from './resolve-path.js'
 
 describe(resolvePath, () => {
   it('should create an absolute path from file://… base url and relative path parts', () => {
-    const path = resolvePath('file://localhost/path/to/foo.txt', 'other/file', 'bar.baz')
+    const basePath = resolve('/path/to/foo.txt')
+    const baseUrl = pathToFileURL(basePath)
+    const resolved = resolvePath(baseUrl, 'other/file', 'bar.baz')
 
-    expect(path).toEqual('/path/to/other/file/bar.baz')
+    expect(resolved).toEqual('/path/to/other/file/bar.baz')
   })
 })
