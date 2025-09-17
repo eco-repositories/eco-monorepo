01fc8898e718a4b08c8b0d50cb7e17bc01791207
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-09-18 00:51:35 +0300

    Use `NODE_ENV=test` in tests

--- packages/server/mock-process-env.ts
+++ packages/server/mock-process-env.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata'
 import { validateConfig } from './src/config/validate-config.js'
+import { EnvName } from './src/config/env-name.js'
 
 const envOriginal = { ...process.env }
 
 const envMock = validateConfig({
+  NODE_ENV: EnvName.test,
+  PORT: '2999',
   CACHE_HOST: 'localhost',
   CACHE_PASS: 'cache_password',
   CACHE_PORT: '6379',
   DB_HOST: 'localhost',
-  DB_NAME: 'development',
+  DB_NAME: 'test',
   DB_PASS: 'database_password',
   DB_PORT: '5432',
   DB_USER: 'postgres',
