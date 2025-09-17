5e42538e5ff6bdf5c54358e48684d31aad7111ad
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-09-17 19:29:06 +0300

    Make DataSourceModule testable

--- packages/server/src/db/data-source/data-source.module.ts
+++ packages/server/src/db/data-source/data-source.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '#@/config/config.module.js'
 import { dataSourceOptionsProvider } from './data-source-options.provider.js'
 import { DbLogger } from './db-logger.js'
 
 @Module({
+  imports: [
+    ConfigModule,
+  ],
   providers: [
     DbLogger,
     dataSourceOptionsProvider,
